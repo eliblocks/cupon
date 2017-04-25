c994d55758fb25d0148aaf8b4976d047de908792
eli block <eliblock@yahoo.com>
2017-04-25 10:28:55 -0700

    print card titles with template strings

--- cupon.js
+++ cupon.js
@@ -1,25 +1,31 @@
 $(function() {
+  data = "";
   getDeals();
+  //printCards(groupon);
 });
 
-
-
 function getDeals() {
-  $.getJSON("sfdeals.json", function(data) {
-    //printDeals(data.deals);
-    returnData(data);
+  $.getJSON("sfdeals.json", function(groupon) {
+    data = groupon;
+    printCards(data)
   });
-}
-
-
-
-function printDeals(data) {
-  $.each(data, function(index, value) {
-    console.log(value);
-  });
-}
-
-function returnData(data) {
   return data;
 }
+
+function cardString(deal) {
+  var cardString = `<div class="col-md-5">
+                      <div class="card deal-card">
+                        <h4 class="card-title deal-title">${deal.merchant.name}</h4>
+                      </div>
+                    </div>`
+  return cardString;
+}
+
+function printCards(data) {
+  $.each(data.deals, function(index, deal) {
+    $("#deal-row").append(cardString(deal));
+  });
+}
+
+
 
