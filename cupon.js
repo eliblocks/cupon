061c149bba113c3699983f6a5c66a724e02c8c62
eli block <eliblock@yahoo.com>
2017-04-25 09:52:19 -0700

    removve test

--- cupon.js
+++ cupon.js
@@ -1,3 +1,25 @@
 $(function() {
-  alert("test javascript");
+  getDeals();
 });
+
+
+
+function getDeals() {
+  $.getJSON("sfdeals.json", function(data) {
+    //printDeals(data.deals);
+    returnData(data);
+  });
+}
+
+
+
+function printDeals(data) {
+  $.each(data, function(index, value) {
+    console.log(value);
+  });
+}
+
+function returnData(data) {
+  return data;
+}
+
